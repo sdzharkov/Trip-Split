d59084bccd6a753d1ae2cbac61f94772b6cfcd95
Andrew Alex <andrewalex123@ucla.edu>
2016-07-27 21:42:16 -0700

    added login and home stub as well as logout functionality

--- tripSlice/settings.py
+++ tripSlice/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tripapp.apps.TripappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Authentication Information
+LOGIN_REDIRECT_URL = '/tripapp/home/'
+
+LOGIN_URL='/login/'
--- tripSlice/urls.py
+++ tripSlice/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
+    url(r'^tripapp/', include('tripapp.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/', auth_views.login, {'template_name':'login.html'},name='login'),
+    url(r'^logout/',auth_views.logout_then_login, name='logout'),
 ]
+
