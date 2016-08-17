1ecdb6db52902f856451abaa6ff7b2f5c2c33625
Andrew Alex <andrewalex123@ucla.edu>
2016-08-17 15:01:37 -0700

    modified the data models

--- tripapp/models.py
+++ tripapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Car(models.Model):
+    car_make = models.CharField(max_length=150)
+    car_model = models.CharField(max_length=150)
+    car_year = models.PositiveSmallIntegerField()
+    car_highway_mpg = models.PositiveSmallIntegerField()
+    car_city_mpg = models.PositiveSmallIntegerField()
+    car_combo_mpg = models.PositiveSmallIntegerField()
+    car_drive = models.CharField(max_length=100)
+    car_fuel_type = models.CharField(max_length=100)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
