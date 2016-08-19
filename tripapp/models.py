945fa76f052f046dfe83d04aa53f7b502612759d
sdzharkov <stephan.zharkov@gmail.com>
2016-08-18 22:59:14 -0700

    Added fixture data for vehicles

--- tripapp/models.py
+++ tripapp/models.py
@@ -7,9 +7,10 @@ class Car(models.Model):
     car_year = models.PositiveSmallIntegerField()
     car_highway_mpg = models.PositiveSmallIntegerField()
     car_city_mpg = models.PositiveSmallIntegerField()
-    car_combo_mpg = models.PositiveSmallIntegerField()
+    car_comb_mpg = models.PositiveSmallIntegerField()
     car_drive = models.CharField(max_length=100)
-    car_fuel_type = models.CharField(max_length=100)
+    car_cylinder = models.PositiveSmallIntegerField()
+    fuel = models.CharField(max_length=100)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
