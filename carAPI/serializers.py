8de43822721158bb9de653041975f3ee76cb09db
sdzharkov <stephan.zharkov@gmail.com>
2017-04-18 20:11:55 -0700

    pushed to heroku

--- carAPI/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from tripapp.models import Car
-
-
-class carSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Car
-        fields = ('car_make', 'car_model', 'car_year', 'car_highway_mpg', 'car_city_mpg', 'car_comb_mpg','car_drive','car_cylinder','fuel')
